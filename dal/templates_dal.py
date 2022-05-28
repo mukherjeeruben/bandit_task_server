a5b978e3595d846d3647e634e2d771253993442a
mukherjeeruben <rubenmukherjee@outlook.com>
2022-05-28 03:08:01 +0100

    Update templates_dal.py

--- dal/templates_dal.py
+++ dal/templates_dal.py
@@ -8,7 +8,17 @@ def get_template_data():
 
 
 def create_template_data():
-    data = [{"name": "Piyush"}, {"name": "Ruben"}]
+    data = [{'0': {'blue': -20, 'red': +40},
+             '1': {'blue': +40, 'red': -60},
+             '2': {'blue': +40, 'red': -60},
+             '3': {'blue': +40, 'red': -60},
+             '4': {'blue': +40, 'red': -60},
+             '5': {'blue': +40, 'red': -60},
+             '6': {'blue': +40, 'red': -60},
+             '7': {'blue': +40, 'red': -60},
+             '8': {'blue': +40, 'red': -60},
+             '9': {'blue': +40, 'red': -60}
+             }]
     collection = 'static_templates'
     result = mdb_execute.execute('create', collection, data)
     return result[0]
