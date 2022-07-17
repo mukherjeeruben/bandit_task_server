3d25d14226879ff11f5015ca4eac8e478546b8c1
Piyush Wadhai <wadhai.piyush@gmail.com>
2022-07-17 10:54:15 +0100

    initial breakdown of extracted data

--- data_extraction_script.py
+++ data_extraction_script.py
@@ -1,5 +1,8 @@
 from interface import mdb_execute
-
+import pandas as pd
+from bson import json_util
+import json
+import csv
 
 def get_all_game_data(query_type):
     collection = 'user_game_data'
@@ -8,8 +11,20 @@ def get_all_game_data(query_type):
 
 
 if __name__ == '__main__':
-    methods = ['voice', 'conventional']
-    for interface_type in methods:
+    method1 = ['voice']
+    method2= ['conventional']
+    result_df1= pd.DataFrame(columns=['X_id','interface_type','user_id','action','reward','total_score'])
+    result_df2 = pd.DataFrame(columns=['X_id', 'interface_type', 'user_id', 'action', 'reward', 'total_score'])
+    print(result_df2)
+    for interface_type in method1:
         query_type = {'interface_type' : interface_type}
         x = get_all_game_data(query_type)
         print(x)
+    for interface_type in method2:
+        query_type = {'interface_type' : interface_type}
+        x = get_all_game_data(query_type)
+        print(len(x))
+        print(x)
+
+
+
