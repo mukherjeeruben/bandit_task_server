8b1b7adb31f754ce928ce43d413f44cd256481c3
mukherjeeruben <rubenmukherjee@outlook.com>
2022-10-03 20:20:29 +0100

    Update data_extraction_script_local.py
    code added to extract demographic data

--- data_extraction_script_local.py
+++ data_extraction_script_local.py
@@ -1,8 +1,23 @@
 import csv
 from config import CSV_FILE_PATH
 from dal.record_game_data_dal import get_user_game_data
+from interface import mdb_execute
 
 user_ids = list()
+
+def get_demographic_data():
+    collection = 'basic_user_data'
+    result = mdb_execute.execute('get', collection)
+    return result
+
+def write_demographic_data_to_csv(data):
+    filepath = CSV_FILE_PATH + '\\' + '_demographic_dataset.csv'
+    with open(filepath, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=',')
+        writer.writerow(["db_id", "User Id", "Gender", "Age"])
+        for row in data:
+            writer.writerow([str(str(row['_id'])), row['user_id'], row['gender'], row['age']])
+
 
 def write_to_csv(task_type, interface_name):
     filepath = CSV_FILE_PATH + '\\' + interface_name + '_dataset.csv'
@@ -24,15 +39,17 @@ def userdata_filer(game_data): # TODO Code to keep uniform dataset for voice and
 
 
 if __name__ == '__main__':
-    game_data = get_user_game_data()
-    task_set = sorted(set([row['interface_type'] for row in game_data]), reverse=True)
-    userdata_filer(game_data)
-    for task_name in task_set:
-        if task_name == 'conventional':
-            data = [row for row in game_data if row['interface_type'] == task_name and row['user_id'] in user_ids]
-        else:
-            data = [row for row in game_data if row['interface_type'] == task_name]
-        write_to_csv(data, task_name)
+    demographic_data = get_demographic_data()
+    write_demographic_data_to_csv(data=demographic_data)
+    # game_data = get_user_game_data()
+    # task_set = sorted(set([row['interface_type'] for row in game_data]), reverse=True)
+    # userdata_filer(game_data)
+    # for task_name in task_set:
+    #     if task_name == 'conventional':
+    #         data = [row for row in game_data if row['interface_type'] == task_name and row['user_id'] in user_ids]
+    #     else:
+    #         data = [row for row in game_data if row['interface_type'] == task_name]
+    #     write_to_csv(data, task_name)
 
 
 
